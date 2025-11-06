0017cc1b60b8b73d0f107dad1c34c7c6972c9d40
AnJunHao <anjunhao_16@outlook.com>
2025-11-06 14:29:11 -0500

    Update rf_grid_search.py

--- rf_grid_search.py
+++ rf_grid_search.py
@@ -25,3 +25,22 @@ for subset in ("root", "non-root"):
         save_file=f"result/{subset.replace('-', '_')}_rf_01.csv",
     )
     best_oversampler = result["micro"]["params"]["oversampler"]
+    df, result = rf_grid_search(
+        dataset,
+        genre_set=subset,
+        oversampler=best_oversampler,
+        min_samples_leaf=1,
+        n_estimators=[10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000],
+        max_depth=None,
+        save_file=f"result/{subset.replace('-', '_')}_rf_02.csv",
+    )
+    best_n_estimators = result["micro"]["params"]["n_estimators"]
+    df, result = rf_grid_search(
+        dataset,
+        genre_set=subset,
+        oversampler=best_oversampler,
+        min_samples_leaf=[1, 2, 3, 4, 5, 6, 7, 8],
+        n_estimators=best_n_estimators,
+        max_depth=None,
+        save_file=f"result/{subset.replace('-', '_')}_rf_03.csv",
+    )
